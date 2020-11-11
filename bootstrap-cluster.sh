7a2ee572cd37f94117ce58cdb5daa8b68b432d02
Henri KOtka <henri.kotka@gmail.com>
2020-11-11 22:45:02 +0200

    Updated script paths

--- bootstrap-cluster.sh
+++ bootstrap-cluster.sh
@@ -3,18 +3,18 @@
 k3d cluster create mycluster --agents 2 -p "30000-32767:30000-32767@server[0]"
 docker exec k3d-mycluster-agent-0 mkdir -p /tmp/kube
 docker exec k3d-mycluster-agent-0 mkdir -p /tmp/kube2
-kubectl apply -f manifests/namespaces/
-kubectl apply -f manifests/volumes/
-kubectl apply -f manifests/configs/
+kubectl apply -f manifests/local/namespaces/
+kubectl apply -f manifests/local/volumes/
+kubectl apply -f manifests/local/configs/
 # Install sealed secrets and apply secrets
 kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.13.1/controller.yaml
 rm manifests/secrets/*
 sleep 30
 kubeseal --scope cluster-wide -o yaml <mysecret-project.json >manifests/secrets/postgres-project-secrets.yaml
 kubeseal --scope cluster-wide -o yaml <mysecret-mainapp.json >manifests/secrets/postgres-mainapp-secrets.yaml
-kubectl apply -f manifests/secrets/
+kubectl apply -f manifests/local/secrets/
 # Start apps
-kubectl apply -f manifests/apps
+kubectl apply -f manifests/local/apps
 # Install monitoring - Grafana, Prometheus, Loki
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add stable https://charts.helm.sh/stable
