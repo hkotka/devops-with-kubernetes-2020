015ce23c0587994b56889b2e1a715706dde3fc07
Henri Kotka <henri.kotka@gmail.com>
2020-11-17 15:09:38 +0200

    Ex 304 (#22)

--- manifests/gke/project/bootstrap.sh
+++ manifests/gke/project/bootstrap.sh
@@ -1,9 +1,6 @@
-kubectl apply -f namespace.yaml
 # Install sealed secrets and apply secrets
 kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.13.1/controller.yaml
 rm secrets/*
 sleep 30
 kubeseal --scope cluster-wide -o yaml <mysecret-project.json >secrets/postgres-project-secrets.yaml
-kubectl apply -f secrets/
-kubectl apply -f ingress.yaml
-kubectl apply -f postgres.yaml
+kubectl apply -k .
